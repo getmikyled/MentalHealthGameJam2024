b0378c293ae4fab156017ad3ebd640fa2b1f0f43
Mikyle Mosquera <mikylemosquera@gmail.com>
2024-06-16 00:54:20 -0700

    Implemented Thought Bubble Sprite

---
+++ MentalHealthGameJam2024/Assets/Scripts/UI/ThoughtBubbleUI.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace MentalHealthGJ_2024
+{
+    ///-////////////////////////////////////////////////////////////////////////
+    ///
+    public class ThoughtBubbleUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+    {
+        ///-////////////////////////////////////////////////////////////////////////
+        ///
+        public void OnPointerEnter(PointerEventData pointerEventData)
+        {
+        
+        }
+    
+        ///-////////////////////////////////////////////////////////////////////////
+        ///
+        public void OnPointerExit(PointerEventData pointerEventData)
+        {
+        
+        }
+    
+        ///-////////////////////////////////////////////////////////////////////////
+        ///
+        public void OnPointerDown(PointerEventData pointerEventData)
+        {
+        
+        }
+    
+        ///-////////////////////////////////////////////////////////////////////////
+        ///
+        public void OnPointerUp(PointerEventData pointerEventData)
+        {
+        
+        }
+    }
+
+}

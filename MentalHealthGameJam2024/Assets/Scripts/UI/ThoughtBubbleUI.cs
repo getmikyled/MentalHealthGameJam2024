6abf664ec4830f7aaf41560bd5376404fa8fe649
Mikyle Mosquera <mikylemosquera@gmail.com>
2024-06-24 00:09:26 -0700

    Implemented moving thought bubbles

--- MentalHealthGameJam2024/Assets/Scripts/UI/ThoughtBubbleUI.cs
+++ MentalHealthGameJam2024/Assets/Scripts/UI/ThoughtBubbleUI.cs
@@ -9,32 +9,45 @@ namespace MentalHealthGJ_2024
     ///
     public class ThoughtBubbleUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
     {
+        private bool isClickedOn = false;
+        
+        ///-////////////////////////////////////////////////////////////////////////
+        ///
+        private void Update()
+        {
+            if (isClickedOn)
+            {
+                transform.position = Input.mousePosition;
+            }
+        }
+        
         ///-////////////////////////////////////////////////////////////////////////
         ///
         public void OnPointerEnter(PointerEventData pointerEventData)
         {
-        
+            Debug.Log("ENTERED");
+            transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
         }
     
         ///-////////////////////////////////////////////////////////////////////////
         ///
         public void OnPointerExit(PointerEventData pointerEventData)
         {
-        
+            transform.localScale = new Vector3(1, 1, 1);
         }
     
         ///-////////////////////////////////////////////////////////////////////////
         ///
         public void OnPointerDown(PointerEventData pointerEventData)
         {
-        
+            isClickedOn = true;
         }
     
         ///-////////////////////////////////////////////////////////////////////////
         ///
         public void OnPointerUp(PointerEventData pointerEventData)
         {
-        
+            isClickedOn = false;
         }
     }
 
